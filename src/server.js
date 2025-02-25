d425f5a8b892e806db2e195c1077f918e8f18780
mridulS <mridul.sehgalsnp@gmail.com>
2025-02-26 00:16:24 +0530

    added origin

--- src/server.js
+++ src/server.js
@@ -11,7 +11,10 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://admin-panel-fe-s7g9.onrender.com",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   },
